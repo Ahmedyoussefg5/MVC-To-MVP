768da115f4e3d1c9259d3dbfe85b3e5d7b71e563
Ahmed Youssef <ahmed.youssef@itgeeks.info>
2019-08-09 10:55:40 +0200

    Initial Commit

---
+++ MVC To MVP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVC To MVP
+//
+//  Created by Youssef on 8/9/19.
+//  Copyright © 2019 Youssef. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
